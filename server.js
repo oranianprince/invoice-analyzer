053d86f7ee6f4b22a35051a9b9d349338064de21
Ryanne Fadel <ryaanefadel@MacBook-Pro-58.local>
2024-07-20 15:44:35 -0400

    Fix port binding for Heroku

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.post('/compare', upload.fields([{ name: 'pdfFile1', maxCount: 1 }, { name: '
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
